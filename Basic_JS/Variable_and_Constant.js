202660172119d1b34e5fa7108e6969ac56b19043
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-08-30 23:31:23 +0900

    Basic_JS

---
+++ Basic_JS/Variable_and_Constant.js
@@ -0,0 +1,19 @@
+let age = 25;
+
+console.log(age);
+
+age = 30;
+
+console.log(age);
+
+var height = 180;
+
+console.log(height);
+
+var height = 170;
+
+console.log(height);
+
+const weight = 80;
+
+console.log(weight);
