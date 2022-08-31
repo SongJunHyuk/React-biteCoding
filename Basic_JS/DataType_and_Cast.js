6b7f5d0df35b61a8f45e29276775fb78e8a9fb9b
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-08-31 23:31:19 +0900

    Basic_JS

---
+++ Basic_JS/DataType_and_Cast.js
@@ -0,0 +1,27 @@
+let age = 25;
+
+let tall = 175.9;
+
+let inf = Infinity;
+
+let minusInf = -Infinity;
+
+let nan = NaN;
+
+let name = "Song";
+
+let name2 = "송";
+
+let name3 = '$(name2) 준혁';
+
+let isSwitchOff = false;
+
+let a = null;
+
+let variable;
+
+let numberA = 12;
+
+let numberB = "2";
+
+console.log(numberA + parseInt(numberB));
