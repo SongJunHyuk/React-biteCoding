99665001e5d132e6824f3ea9992f8eb17731b2cf
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-09-01 23:42:14 +0900

    Basic_JS

---
+++ Basic_JS/Operator.js
@@ -0,0 +1,31 @@
+let a = 1;
+
+let b = 2;
+
+console.log(a + b);
+
+console.log(a * b);
+
+console.log(a - b);
+
+console.log(a / b);
+
+console.log(a % b);
+
+console.log(!false);
+
+console.log(true || false);
+
+let compareA = 1 == "1";
+
+let compareB = 1 === "1";
+
+console.log(compareA);
+
+console.log(consoleB);
+
+console.log(typeof compareA);
+
+let n = n ?? 10;
+
+console.log(n);
