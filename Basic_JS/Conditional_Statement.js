1f2ff3ab3410c358252367d8e2547c445545d7d7
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-09-02 21:39:04 +0900

    Basic_JS

---
+++ Basic_JS/Conditional_Statement.js
@@ -0,0 +1,24 @@
+let a = 3;
+
+if ( a >= 7 ) {
+    console.log("7 이상입니다.");
+} else if(a >= 5 ) {
+    console.llg("5 이상입니다.");
+} else {
+    console.log("5 미만입니다.");
+}
+
+let country = "ko";
+
+if(country === "ko") {
+    console.log("한국");
+}
+else if(country === "cn") {
+    console.log("중국");
+}
+else if(country === "jp") {
+    console.log("일분");
+}
+else {
+    console.log("미 분류");
+}
