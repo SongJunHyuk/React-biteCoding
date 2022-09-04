d00e8d437670b1dd3f037e5a7c536511fa22479c
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-09-04 23:26:18 +0900

    Basic_JS

---
+++ Basic_JS/Function.js
@@ -0,0 +1,15 @@
+function getArea(width, height) {
+    let area = width * height;
+    console.log(area);
+}
+
+getArea(1, 200);
+getArea(2, 200);
+getArea(3, 200);
+getArea(4, 200);
+getArea(5, 200);
+getArea(6, 200);
+getArea(7, 200);
+getArea(8, 200);
+getArea(9, 200);
+getArea(10, 200);
